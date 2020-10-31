cd6193c9e5475b33e9110527452fe7c0c394e583
ippikioukami <memacias2@miners.utep.edu>
2020-10-31 15:56:57 -0600

    Merger and Headers

---
+++ project/src/stateMachines.h
@@ -0,0 +1,12 @@
+#ifndef stateMachines_included
+#define stateMachine_included
+
+void state_advance();
+char toggle_red();
+char toggle_green();
+void up_state();
+void down_state();
+void buzzer_advance();
+void dimLights(char x);
+
+#endif
